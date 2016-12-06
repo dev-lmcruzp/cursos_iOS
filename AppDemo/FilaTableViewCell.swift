3bce943faedfbe78de7ce09f1dfee6dc70a3f69a
STI <STI@MacBook-Pro-de-STI.local>
2016-12-06 09:56:58 -0600

    iniciando los comino

---
+++ AppDemo/FilaTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FilaTableViewCell.swift
+//  AppDemo
+//
+//  Created by STI on 05/12/16.
+//  Copyright © 2016 integrait. All rights reserved.
+//
+
+import UIKit
+
+class FilaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblIzquierda: UILabel!
+    @IBOutlet weak var lblDerecha: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- AppDemo/ViewController.swift
+++ AppDemo/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var btnBoton: UIButton!
 
+    let datos = [("Luis", 24), ("Miguel", 25)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return datos.count
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let filaInpar = (indexPath.row % 2 == 0)
+        let proto = filaInpar ? "proto1" : "proto2"
+        
+        let vista = tableView.dequeueReusableCell(withIdentifier: proto, for: indexPath) as! FilaTableViewCell
+        
+        if filaInpar{
+            vista.lblDerecha.text = "\(datos[indexPath.row].1)"
+            vista.lblIzquierda.text = datos[indexPath.row].0
+        }else{
+            vista.lblIzquierda.text = "\(datos[indexPath.row].1)"
+            vista.lblDerecha.text = datos[indexPath.row].0
+        }
+        
+        
+        return vista
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Fila \(indexPath.row)")
+    }
 
 }
 
